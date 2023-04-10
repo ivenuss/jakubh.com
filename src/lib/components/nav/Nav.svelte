833e71d696c46d07d45b661ee9edda5df5b4f579
venus <jhabrcetl@seznam.cz>
2023-04-10 18:13:38 +0200

    Fix mobile

--- src/lib/components/nav/Nav.svelte
+++ src/lib/components/nav/Nav.svelte
@@ -40,16 +40,16 @@
 	</button>
 </nav>
 
-<div class="absolute bottom-0 left-0 right-0 top-16 z-50 block md:hidden">
-	<Transition show={expanded}>
-		<Transition
-			enter="transition ease-in-out duration-300 transform"
-			enterFrom="translate-x-full"
-			enterTo="translate-x-0"
-			leave="transition ease-in-out duration-300 transform"
-			leaveFrom="translate-x-0"
-			leaveTo="translate-x-full"
-		>
+<Transition show={expanded}>
+	<Transition
+		enter="transition ease-in-out duration-300 transform"
+		enterFrom="translate-x-full"
+		enterTo="translate-x-0"
+		leave="transition ease-in-out duration-300 transform"
+		leaveFrom="translate-x-0"
+		leaveTo="translate-x-full"
+	>
+		<div class="absolute bottom-0 left-0 right-0 top-16 z-50 block md:hidden">
 			<aside class="h-full w-full bg-white dark:bg-black">
 				<ul class="mx-auto flex max-w-screen-sm flex-col gap-1.5 px-4">
 					{#each links as { href, label }}
@@ -57,6 +57,6 @@
 					{/each}
 				</ul>
 			</aside>
-		</Transition>
+		</div>
 	</Transition>
-</div>
+</Transition>
