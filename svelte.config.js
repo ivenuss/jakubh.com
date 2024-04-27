27b2c1defa49273194d871082eabb2b374d58a82
venus <jhabrcetl@gmail.com>
2024-04-27 17:58:12 +0200

    major(svelte-5): Bump to Svelte 5 version

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+	preprocess: [mdsvex(mdsvexConfig)],
 	kit: {
 		adapter: adapter()
 	}
