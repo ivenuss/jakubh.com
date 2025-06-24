17e4ad0bb9822e290a3dcaf048e30eb23670c620
venus <jhabrcetl@gmail.com>
2025-06-24 19:39:59 +0200

    feat(vite): migrate to vite rolldown (#103)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import svg from '@poppanator/sveltekit-svg';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'rolldown-vite';
 
-export default {
-	plugins: [sveltekit(), tailwindcss(), svg()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-};
+export default defineConfig({
+	plugins: [sveltekit(), tailwindcss(), svg()]
+});
