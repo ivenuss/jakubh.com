e17c4195e01507aa3b7c5e75275123edd94940ce
venus <jhabrcetl@gmail.com>
2025-09-08 19:44:22 +0200

    feat(remote-fn): add testing page

---
+++ src/routes/(main)/test/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { createName } from './data.remote';
+</script>
+
+<form {...createName}>
+	<input type="text" name="name" />
+	<button type="submit">Submit</button>
+</form>
+
+{#if createName.result?.greeting}
+	<div>{createName.result.greeting}</div>
+{/if}
