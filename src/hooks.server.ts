df554750c460fd2ff6e3afa0abf375f6c35bb887
venus <jhabrcetl@gmail.com>
2025-09-16 19:30:08 +0200

    twks

--- src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
-
-// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
