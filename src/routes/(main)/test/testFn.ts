0100feb67a6b86c359afc7f1ee438cc605521023
venus <jhabrcetl@gmail.com>
2026-07-16 22:02:57 +0200

    remove test fn

--- src/routes/(main)/test/testFn.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const items: any[] = [undefined];
-
-export function testFn(): boolean {
-	return items.find((item) => item.aha === 'test') !== undefined;
-}
